3f6c492f2fc786befa6fd54dc7aff19fb05c84dc
domabo <admin@domabo.com>
2015-04-13 11:45:00 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 var wolfscript;
+var util = require('util');
 
 exports.enable = function (){ 
-    this.getLogman().info("HELLO ENABLE");
- };
+    this.getLogman().info("HELLO WOLFPACK");
+ 
+    this.registerCommand({
+      "aliases" : ["js"]
+     , "description" : "execute a javascript command"
+     , "toolTip" : "/js command"
+     , execute: function(sender, args) {
+        __log.info("TESTY");
+        __log.info(this);
+        __log.info(sender);
+        __log.info(args);
+        this.getLogman().info("Command js received");
+     }
+    });
+    
+    var that = this;
+    this.events.on("LoadWorld", function(e){
+        that.getLogman().info("io.wolfscript.hook.system.LoadWorldHook");
+        that.getLogman().info(e.getWorld().getFqName());
+        that.getLogman().info(e.getWorld());
+        that.getLogman().info(e);
+    });
+ }; 
  
